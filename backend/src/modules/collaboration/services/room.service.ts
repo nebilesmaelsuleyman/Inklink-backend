60ca069964417b9ddec7ab814fe9e47ee486e60e
nebilesmaelsuleyman <nebilesmaelprog@gmail.com>
2026-03-19 13:40:41 +0300

    [feat]broadcast messages

--- backend/src/modules/collaboration/services/room.service.ts
+++ backend/src/modules/collaboration/services/room.service.ts
@@ -34,13 +34,26 @@ export class RoomService {
         }
         return room;
     }
-    
+
     private encodeMessage(type:MessageType,payload:Uint8Array){
         const encoder = encoding.createEncoder();
         encoding.writeVarUint(encoder,type);
         encoding.writeVarUint8Array(encoder,payload);
         return encoding.toUint8Array(encoder);
 
+    }
+    
+    broadcast(room:IRoom, message:Uint8Array){
+        room.connections.forEach((conn)=>{
+            if(conn.readyState === WebSocket.OPEN){
+                try{
+                    conn.send(message);
+                }catch{
+                    conn.close();
+                    room.connections.delete(conn);
+                }
+            }
+        })
     }
 
 }
