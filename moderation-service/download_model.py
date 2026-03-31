cba267538de70a37a05ce1da0dc89d019a286a47
nebilesmaelsuleyman <nebilesmaelprog@gmail.com>
2026-03-31 11:02:24 +0300

    feat: add Dockerfile for application containerization and environment setup

--- moderation-service/download_model.py
+++ moderation-service/download_model.py
@@ -1,20 +1,16 @@
 import gdown
-import zipfile
 import os
 
 FOLDER_ID = "1_mqtiVMW6otZ0DrWZyUoZgxs4Bnu_xtd"
 URL = f"https://drive.google.com/uc?id={FOLDER_ID}"
 
-OUTPUT = "model_checkpoint.pt"
-
 if not os.path.exists("model"):
     print("Downloading model...")
     gdown.download_folder(
-    id=FOLDER_ID,
-    output="model",
-    quiet=False
-)
-
+        id=FOLDER_ID,
+        output="model",
+        quiet=False,
+    )
     print("Model ready")
 else:
     print("Model already exists")
