9db9a3a51e9f47f447ffe76a3e7036b451578667
mamacite <nezif223@gmail.com>
2026-04-25 14:00:00 +0300

    feat(backend): configure chapters module

--- backend/src/modules/chapters/chapters.module.ts
+++ backend/src/modules/chapters/chapters.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { LibraryModule } from '../library/library.module';
 import { ModerationModule } from '../moderation/moderation.module';
 import { NotificationsModule } from '../notifications/notifications.module';
+import { CollaborationModule } from '../collaboration/collaboration.module';
+import { SubscriptionModule } from '../subscription/subscription.module';
 import { USER_MODEL_NAME, UserSchema } from '../users/user.schema';
 import { WORK_MODEL_NAME, WorkSchema } from '../works/schema/work.schema';
 import { ChaptersController } from './chapters.controller';
@@ -14,6 +16,8 @@ import { CHAPTER_MODEL_NAME, ChapterSchema } from './schema/chapter.schema';
     ModerationModule,
     LibraryModule,
     NotificationsModule,
+    forwardRef(() => CollaborationModule),
+    forwardRef(() => SubscriptionModule),
     MongooseModule.forFeature([
       { name: CHAPTER_MODEL_NAME, schema: ChapterSchema },
       { name: WORK_MODEL_NAME, schema: WorkSchema },
