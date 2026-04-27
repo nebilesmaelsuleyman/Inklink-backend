e5ce118e80daef18cb79bcb06921d622b1723ddc
mamacite <mamacite@users.noreply.github.com>
2026-04-28 00:23:43 +0300

    feat(backend): enhance profile and user services with auto-creation

--- backend/src/modules/profile/profile.module.ts
+++ backend/src/modules/profile/profile.module.ts
@@ -3,12 +3,16 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ProfileService } from './profile.service';
 import { ProfileController } from './profile.controller';
 import { PROFILE_MODEL_NAME, ProfileSchema } from './profile.model';
+import { UsersModule } from '../users/users.module';
+import { WorksModule } from '../works/works.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: PROFILE_MODEL_NAME, schema: ProfileSchema },
     ]),
+    UsersModule,
+    WorksModule,
   ],
   controllers: [ProfileController],
   providers: [ProfileService],
