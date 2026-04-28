2c1758f79700ad9bd49f08cfbb5c4c086fe2bd6a
Ezexkedir10 <ezexkedir21@gmail.com>
2026-04-28 10:00:00 +0300

    feat(backend): setup works module

--- backend/src/modules/works/works.module.ts
+++ backend/src/modules/works/works.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ChaptersModule } from '../chapters/chapters.module';
 import { ModerationModule } from '../moderation/moderation.module';
@@ -6,13 +6,21 @@ import { ReactionsModule } from '../reactions/reactions.module';
 import { WORK_MODEL_NAME, WorkSchema } from './schema/work.schema';
 import { WorksController } from './works.controller';
 import { WorksService } from './works.service';
+import { CollaborationModule } from '../collaboration/collaboration.module';
+import { NotificationsModule } from '../notifications/notifications.module';
+import { ProfileSchema } from '../profile/profile.model';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: WORK_MODEL_NAME, schema: WorkSchema }]),
+    MongooseModule.forFeature([
+      { name: WORK_MODEL_NAME, schema: WorkSchema },
+      { name: 'Profile', schema: ProfileSchema },
+    ]),
     ChaptersModule,
     ReactionsModule,
     ModerationModule,
+    NotificationsModule,
+    forwardRef(() => CollaborationModule),
   ],
   controllers: [WorksController],
   providers: [WorksService],
