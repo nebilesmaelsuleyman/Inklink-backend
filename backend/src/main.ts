eb6a48fd7320e2f6923552bbb286929ed4c440c7
nebil esmael <nebilesmaelprog@gmail.com>
2026-05-26 13:14:00 +0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -31,6 +31,7 @@ async function bootstrap() {
     'http://127.0.0.1:3000',
     'http://localhost:3001',
     'http://127.0.0.1:3001',
+    "*",
     frontendUrl,
     ...explicitCorsOrigins,
   ]);
